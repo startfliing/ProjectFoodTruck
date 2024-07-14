fb219971ed733979c651d08249a215d171b211e6
jbhelgas <jbhelgas@asu.edu>
2024-07-14 14:31:29 -0700

    Tonc GBA Template - pure copy and paste

---
+++ build/main.d
@@ -0,0 +1,32 @@
+main.o: \
+ C:/devkitPro/libtonc-examples/libtonc-examples-master/lab/template/source/main.c \
+ C:/devkitPro/libtonc/include/tonc.h \
+ C:/devkitPro/libtonc/include/tonc_types.h \
+ C:/devkitPro/libtonc/include/tonc_memmap.h \
+ C:/devkitPro/libtonc/include/tonc_memdef.h \
+ C:/devkitPro/libtonc/include/tonc_bios.h \
+ C:/devkitPro/libtonc/include/tonc_core.h \
+ C:/devkitPro/libtonc/include/tonc_input.h \
+ C:/devkitPro/libtonc/include/tonc_irq.h \
+ C:/devkitPro/libtonc/include/tonc_math.h \
+ C:/devkitPro/libtonc/include/tonc_oam.h \
+ C:/devkitPro/libtonc/include/tonc_tte.h \
+ C:/devkitPro/libtonc/include/tonc_surface.h \
+ C:/devkitPro/libtonc/include/tonc_video.h \
+ C:/devkitPro/libtonc/include/tonc_nocash.h \
+ C:/devkitPro/libtonc/include/tonc_text.h
+C:/devkitPro/libtonc/include/tonc.h:
+C:/devkitPro/libtonc/include/tonc_types.h:
+C:/devkitPro/libtonc/include/tonc_memmap.h:
+C:/devkitPro/libtonc/include/tonc_memdef.h:
+C:/devkitPro/libtonc/include/tonc_bios.h:
+C:/devkitPro/libtonc/include/tonc_core.h:
+C:/devkitPro/libtonc/include/tonc_input.h:
+C:/devkitPro/libtonc/include/tonc_irq.h:
+C:/devkitPro/libtonc/include/tonc_math.h:
+C:/devkitPro/libtonc/include/tonc_oam.h:
+C:/devkitPro/libtonc/include/tonc_tte.h:
+C:/devkitPro/libtonc/include/tonc_surface.h:
+C:/devkitPro/libtonc/include/tonc_video.h:
+C:/devkitPro/libtonc/include/tonc_nocash.h:
+C:/devkitPro/libtonc/include/tonc_text.h:
